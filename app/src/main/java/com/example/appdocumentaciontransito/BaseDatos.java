161e459f2fe508b67d2992c7bb4a0a2ec4dcd571
AlondraBlanco7 <alondrasanchez3679@gmail.com>
2023-06-09 08:11:27 -0600

    Creación de tablas bd

--- app/src/main/java/com/example/appdocumentaciontransito/BaseDatos.java
+++ app/src/main/java/com/example/appdocumentaciontransito/BaseDatos.java
@@ -15,8 +15,16 @@ public class BaseDatos extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE propietario(curp text PRIMARY KEY, nombre text,paterno text, materno text, fecha_nacimiento text, sexo text, telefono text, domicilio text)");
-        db.execSQL("CREATE TABLE usuario(username text PRIMARY KEY, contrasenia text,curp_propietario text, FOREIGN KEY(curp_propietario) REFERENCES propietario(curp))");
+        db.execSQL("CREATE TABLE propietario(curp text PRIMARY KEY, nombre text, paterno text, materno text, fecha_nacimiento text, sexo text, telefono text, domicilio text)");
+        db.execSQL("CREATE TABLE usuario(username text PRIMARY KEY, contrasenia text, curp_propietario text, FOREIGN KEY(curp_propietario) REFERENCES propietario(curp))");
+        db.execSQL("create table licencia(numero_licencia text primary key, tipo text, fecha_emision text, fecha_expiracion text, estado_emision text, imagen_licencia text, curp_propietario text, foreign key(curp_propietario) references propietario(curp))");
+        db.execSQL("create table vehiculo(numero_serie text primary key, numero_placas text, marca text, modelo text, anio text, tipo text, numero_motor text, curp_propietario text, foreign key(curp_propietario) references propietario(curp))");
+        db.execSQL("create table tarjeta_circulacion(numero_serie_tarjeta text primary key, fecha_emision text, fecha_expiracion text, estado_emision text, imagen_tarjeta text, numero_serie_vehiculo text, foreign key(numero_serie_vehiculo) references vehiculo(numero_serie))");
+        db.execSQL("CREATE TABLE estado(id INTEGER PRIMARY KEY AUTOINCREMENT, nombre text)");
+        db.execSQL("CREATE TABLE tipo_multa(id INTEGER PRIMARY KEY AUTOINCREMENT, tipo text)");
+        /* SEGÚN EL ESTADO MOSTRAR UN COMBOBOX CON LAS DIFERENTES MULTAS */
+        db.execSQL("CREATE TABLE tipo_multa_estado(id INTEGER PRIMARY KEY AUTOINCREMENT, monto real, tipo_multa_id INTEGER, estado_id INTEGER, FOREIGN KEY(tipo_multa_id) REFERENCES tipo_multa(id), FOREIGN KEY(estado_id) REFERENCES estado(id))");
+        db.execSQL("CREATE TABLE multa(id INTEGER PRIMARY KEY AUTOINCREMENT, fecha_multa text, fecha_limite text, monto_multa real, municipio text, status INTEGER, multa_estado_id INTEGER, FOREIGN KEY(multa_estado_id) REFERENCES tipo_multa_estado(id))");
     }
 
     @Override
